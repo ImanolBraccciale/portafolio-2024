a8fa966f240c4c55d86479aef4b26e0f9eb46743
ImanolBraccciale <imanol11699@outlook.com>
2024-05-14 19:01:17 -0300

    zzzzzzzzzzzzzzzzzzzzzzzz

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       projects: 'Proyectos',
       socialMedia: 'Redes Sociales',
       description: 'Desarrollador FullStack amante de los retos, proyectos nuevos y códigos novedosos.',
-      cv:"https://drive.google.com/file/d/1teB2EZv7I4ThT2q-lZJOsYqWkl1nfmRR/view?usp=drive_link"
+      cv:"https://drive.google.com/file/d/1tEd1WQKddthLnE0aMeiGfk48WtWJqpe5/view?usp=drive_link"
     },
     en: {
       aboutMe: 'About Me',
